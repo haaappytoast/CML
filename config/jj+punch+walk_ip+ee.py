96a54cfefe0256fe8eac3c08dbe62401aaf9f0d2
HappyToast <yrs1107@naver.com>
2023-07-25 21:02:28 +0900

    config/jj+punch+walk_ip+ee.py changed
    jumping jack: left arm
    punch: right arm
    walk_ip: lower body

--- config/jj+punch+walk_ip+ee.py
+++ config/jj+punch+walk_ip+ee.py
@@ -13,22 +13,22 @@ training_params = dict(
 )
 
 discriminators = {
-    "front_jumping_jack/upper": dict(
+    "front_jumping_jack/left_arm": dict(
         motion_file = "assets/motions/gym/front_jumping_jack.json",
-        key_links = ["torso", "head", "right_upper_arm", "right_lower_arm", "right_hand"],
-        parent_link = "pelvis",
-        local_pos = True,
-        replay_speed = lambda n: np.random.uniform(0.8, 1.2, size=(n,))
-    ),
-    "punch/left_arm": dict(
-        motion_file = "assets/motions/iccgan/punch.json",
         key_links = ["left_upper_arm", "left_lower_arm", "left_hand"],
         parent_link = "pelvis",
         local_pos = True,
         replay_speed = lambda n: np.random.uniform(0.8, 1.2, size=(n,))
     ),
+    "punch/upper": dict(
+        motion_file = "assets/motions/iccgan/punch.json",
+        key_links = ["right_upper_arm", "right_lower_arm", "right_hand"],
+        parent_link = "pelvis",
+        local_pos = True,
+        replay_speed = lambda n: np.random.uniform(0.8, 1.2, size=(n,))
+    ),
     "walk_in_place/lower": dict(
-        key_links = ["pelvis", "right_thigh", "right_shin", "right_foot", "left_thigh", "left_shin", "left_foot"],
+        key_links = ["pelvis", "torso", "head", "right_thigh", "right_shin", "right_foot", "left_thigh", "left_shin", "left_foot"],
         parent_link = None
     )
 }
