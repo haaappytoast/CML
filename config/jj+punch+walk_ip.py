706c5bf8e853d30e91d8b0c11a174556570c6020
HappyToast <yrs1107@naver.com>
2023-09-04 16:51:25 +0900

    partwise discriminator

--- config/jj+punch+walk_ip.py
+++ config/jj+punch+walk_ip.py
@@ -15,14 +15,14 @@ training_params = dict(
 discriminators = {
     "front_jumping_jack/upper": dict(
         motion_file = "assets/motions/gym/front_jumping_jack.json",
-        key_links = ["torso", "head", "right_upper_arm", "right_lower_arm"],
+        key_links = ["torso", "head", "left_upper_arm", "left_lower_arm", "left_hand"],
         parent_link = "pelvis",
         local_pos = True,
         replay_speed = lambda n: np.random.uniform(0.8, 1.2, size=(n,))
     ),
     "punch/right_arm": dict(
         motion_file = "assets/motions/iccgan/punch.json",
-        key_links = ["left_upper_arm", "left_lower_arm", "left_hand"],
+        key_links = ["right_upper_arm", "right_lower_arm", "right_hand"],
         parent_link = "pelvis",
         local_pos = True,
         replay_speed = lambda n: np.random.uniform(0.8, 1.2, size=(n,))
@@ -30,5 +30,11 @@ discriminators = {
     "walk_in_place/lower": dict(
         key_links = ["pelvis", "right_thigh", "right_shin", "right_foot", "left_thigh", "left_shin", "left_foot"],
         parent_link = None
-    )    
+    )
 }
+
+# {'head': 2, 'left_foot': 14, 'left_hand': 8, 
+# 'left_lower_arm': 7, 'left_shin': 13, 'left_thigh': 12, 
+# 'left_upper_arm': 6, 'pelvis': 0, 'right_foot': 11, 
+# 'right_hand': 5, 'right_lower_arm': 4, 'right_shin': 10, 
+# 'right_thigh': 9, 'right_upper_arm': 3, 'torso': 1} 15
