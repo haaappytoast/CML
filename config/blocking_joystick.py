b15b571d1402542d87dcf0f53df468a8739fafc1
HappyToast <yrs1107@naver.com>
2023-11-14 21:56:37 +0900

    dataset added for block

--- config/blocking_joystick.py
+++ config/blocking_joystick.py
@@ -1,5 +1,5 @@
 import numpy as np
-# python main.py config/blocking_joystick.py --ckpt 1113_block_MIX+REAL --headless --server local
+# python main.py config/blocking_joystick.py --ckpt 1114_block_MIX+REAL_embed16 --headless --server local
 env_cls = "ICCGANHumanoidVRControl"
 env_params = dict(
     episode_length = 300,
@@ -18,7 +18,7 @@ env_params = dict(
 )
 
 training_params = dict(
-    max_epochs = 30000,
+    max_epochs = 60000,
     save_interval = 5000,
     terminate_reward = -1
 )
@@ -49,7 +49,8 @@ discriminators = {
     "usermotion1/upper": dict(
         # motion_file = "assets/retargeted/test/blocking_test/cml@outward_block+blocking2.npy",
         # motion_file = "assets/retargeted/test/blocking_test/cml@blocking2.npy",
-        motion_file = "assets/retargeted/1114_block.yaml",
+        motion_file = "assets/retargeted/1114_block_MIX+REAL.yaml",
+        # motion_file = "assets/retargeted/1114_userblock/cml@user_outblock_TEST.npy",
         key_links = ["torso", "head", "right_upper_arm", "right_lower_arm", "right_hand", "left_upper_arm", "left_lower_arm", "left_hand"],
         parent_link = "pelvis",
         ob_horizon = 3,
